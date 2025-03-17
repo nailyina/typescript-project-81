f330eb53682cbafa0df6c2d8cc2ec0fb22edb8ac
n.d.ilyina <n.d.ilyina@tbank.ru>
2025-03-17 21:11:12 +0300

    добавить тесты

---
+++ __tests__/tagGenerator.test.ts
@@ -0,0 +1,18 @@
+import { expect, test } from "vitest";
+import { Tag } from "../src/tagGenerator";
+
+test("simple tag generation", () => {
+    expect(new Tag("br").toString()).toBe("<br>");
+});
+
+test("tags with attributes", () => {
+    expect(new Tag("input", { type: "submit", value: "Save" }).toString()).toBe(
+        `<input type="submit" value="Save"`
+    );
+});
+
+test("tags with attributes and value", () => {
+    expect(new Tag("label", { for: "email" }, "Email").toString()).toBe(
+        `<label for="email">Email</label>`
+    );
+});
--- src/TagGenerator.ts
+++ src/TagGenerator.ts
@@ -4,11 +4,13 @@ type TagAttribute = {
 
 export class Tag {
     private tagName: string;
-    private tagAttributes: TagAttribute;
+    private tagAttributes?: TagAttribute;
+    private value?: string;
 
-    constructor(tagName: string, tagAttributes: TagAttribute) {
+    constructor(tagName: string, tagAttributes?: TagAttribute, value?: string) {
         this.tagName = tagName;
         this.tagAttributes = tagAttributes;
+        this.value = value;
     }
 
     public toString(): string {
@@ -16,6 +18,10 @@ export class Tag {
 
         for (const key in this.tagAttributes) {
             tag += ` ${key}="${this.tagAttributes[key]}"`;
+        }
+
+        if (this.value) {
+            tag += `>${this.value}</${this.tagName}`;
         }
 
         tag += `>`;
