482b9e1ec85e026ebe1f26c177ebeb2d20ef7d8c
n.d.ilyina <n.d.ilyina@tbank.ru>
2025-03-18 17:52:43 +0300

    правки номер 100000000

---
+++ src/tagGenerator.ts
@@ -0,0 +1,29 @@
+type TagAttribute = Record<string, string>
+
+export class Tag {
+  private tagName: string
+  private tagAttributes?: TagAttribute
+  private value?: string
+
+  constructor(tagName: string, tagAttributes?: TagAttribute, value?: string) {
+    this.tagName = tagName
+    this.tagAttributes = tagAttributes
+    this.value = value
+  }
+
+  public toString(): string {
+    let tag = `<${this.tagName}`
+
+    for (const key in this.tagAttributes) {
+      tag += ` ${key}="${this.tagAttributes[key]}"`
+    }
+
+    if (this.value) {
+      tag += `>${this.value}</${this.tagName}`
+    }
+
+    tag += `>`
+
+    return tag
+  }
+}
